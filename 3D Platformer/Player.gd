da5eaf21accbcfcfa74b757fe4ecdfddceaf4232
mSebbi <75531894+mSebbi@users.noreply.github.com>
2021-09-14 10:48:47 +0200

    Bessere bewegung , Physics und funktion Springen
    Der Spieler bleibt jetzt auf dem Boden und kann Springen

--- 3D Platformer/Player.gd
+++ 3D Platformer/Player.gd
@@ -1,7 +1,23 @@
 extends KinematicBody
 
 const MOUS_SENSIVITY = 0.3
+onready var camera = $CamRoot/Camera
 
+#Variablen für die Bewegung
+var velocity = Vector3.ZERO
+var current_vel = Vector3.ZERO
+var direction = Vector3.ZERO
+
+const SPEED = 10
+const SPRINT_SPEED = 20
+const ACCEL = 15.0
+
+#Springen
+const GRAVITY = -40
+const JUMP_SPEED = 15
+var jump_counter = 0
+const AIR_ACCEL = 9.0
+ 
 
 
 func _process(delta):
@@ -25,7 +41,45 @@ func _input(event):
 		
 
 func _physics_process(delta):
-	pass
+	direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("forward"):
+		direction -= camera.global_transform.basis.z
+	if Input.is_action_pressed("backward"):
+		direction += camera.global_transform.basis.z
+	if Input.is_action_pressed("right"):
+		direction += camera.global_transform.basis.x
+	if Input.is_action_pressed("left"):
+		direction -= camera.global_transform.basis.x
+	
+	direction = direction.normalized()
+	
+	velocity.y += GRAVITY * delta
+	
+	if is_on_floor():
+		jump_counter = 0
+		
+	if Input.is_action_just_pressed("jump") and jump_counter < 2:
+		jump_counter += 1
+		velocity.y = JUMP_SPEED
+		
+		 
+	
+	var speed = SPRINT_SPEED if Input.is_action_pressed("sprint") else SPEED
+	var target_vel = direction * speed
+	
+	var accel = ACCEL if is_on_floor() else AIR_ACCEL
+	
+	current_vel = current_vel.linear_interpolate(target_vel, accel * delta)
+	velocity.x = current_vel.x
+	velocity.z = current_vel.z
+	
+	velocity = move_and_slide(velocity, Vector3.UP,true, 4, deg2rad(45))
+		
+	
+	
+	
+	
 
 #Um die Maus zu zeigen oder zu verstecken
 func window_activity():
